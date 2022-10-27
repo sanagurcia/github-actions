3ff8b4570cc277d7715814b5e598b690ecd20485
Santiago <sanagurcia@gmail.com>
2022-10-27 21:22:48 +0200

    response change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.json({ msg: 'Servus!' })
+    res.json({ msg: 'Howdy!' })
 })
 
 app.listen(8000, () => {
